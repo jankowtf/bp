83d05a1a7487113bb1eca02feeae2544ab910af9
Janko Thyson <janko.thyson@rappster.de>
2019-10-18 15:02:12 +0200

    Function pretty_get_mapping_table_accounting()

--- R/pretty.R
+++ R/pretty.R
@@ -23,3 +23,23 @@ pretty_get_mapping_table <- function(
     dplyr::select({{ col_from }} , {{ col_to }}) %>%
     dplyr::filter({{ col_from }} %in% values)
 }
+
+#' @export
+pretty_get_mapping_table_accounting <- function(
+  mapping_table_parent,
+  values,
+  accounting = FALSE,
+  .col_from = bp::get_config("column_names/col_name") %>%
+    bp::symbolize(),
+  .col_to =bp::get_config("column_names/col_name_pretty_accounting") %>%
+    bp::symbolize()
+  # ...
+) {
+  col_from
+  col_to
+
+  # cols <- rlang::list2(...)
+  mapping_table_parent %>%
+    dplyr::select({{ col_from }} , {{ col_to }}) %>%
+    dplyr::filter({{ col_from }} %in% values)
+}
